9cdffb31902f77b2b5bf502018ce44cf2e6a8e80
marcusformiga <kiko.antares@gmail.com>
2021-08-31 10:52:47 -0300

    feature: criação da interface e do repositorio de usuarios

---
+++ src/module/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,23 @@
+import { getRepository, Repository } from "typeorm";
+import { CreateUserDto } from "../../entities/dto/CreateUserDto";
+import { User } from "../../entities/User";
+import { ICreateUserRepository } from "../interfaces/ICreateUserRepository";
+
+export class UsersRepository implements ICreateUserRepository {
+  userRepository: Repository<User>;
+  constructor() {
+    this.userRepository = getRepository(User);
+  }
+  async create({ name, email, password }: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    this.userRepository.create(user);
+    await this.userRepository.save(user);
+    return user;
+  }
+  findByEmail(email: string): Promise<User | undefined> {
+    throw new Error("Method not implemented.");
+  }
+}
