88acb46fbe65737d71e2fc8ed9d758da0937d852
marcusformiga <kiko.antares@gmail.com>
2021-08-31 09:20:20 -0300

    feature: criação da classe de erro da api

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,8 @@
+export class AppError {
+  message: string;
+  statusCode: number;
+  constructor(message: string, statusCode: number = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
