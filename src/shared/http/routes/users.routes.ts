35669ec79447f3ac0d1789d743467da588b122ff
marcusformiga <kiko.antares@gmail.com>
2021-09-01 13:18:40 -0300

    feature: adição da rota para listar usuarios

--- src/shared/http/routes/users.routes.ts
+++ src/shared/http/routes/users.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { CreateUsersController } from "../../../module/users/useCases/createUser/CreateUsersController";
+import { ListAllUsersController } from "../../../module/users/useCases/listUsers/ListAllUsersController";
+
 export const usersRouter = Router();
 const createUsersController = new CreateUsersController();
-
+const listAllUsersController = new ListAllUsersController();
 usersRouter.post("/", createUsersController.handle);
+usersRouter.get("/", listAllUsersController.handle);
