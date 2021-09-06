58d44a6afd1a635b3ffe45647c2ed4a18a2f8baa
marcusformiga <kiko.antares@gmail.com>
2021-09-06 08:23:30 -0300

    test: criação dos testes de deleção de usuarios

---
+++ src/__tests__/DeleteUsersUseCases.spec.ts
@@ -0,0 +1,29 @@
+import { UsersRepositoryFake } from "../module/users/repositories/fakes/UsersRepositoryFake";
+import { DeleteUsersUseCases } from "../module/users/useCases/DeleteUsers/DeleteUsersUseCases";
+import { AppError } from "../shared/errors/AppError";
+
+let deleteUsersUseCases: DeleteUsersUseCases;
+let usersRepositoryFake: UsersRepositoryFake;
+
+describe("DeleteUsersUseCases", () => {
+  beforeEach(() => {
+    deleteUsersUseCases = new DeleteUsersUseCases(usersRepositoryFake);
+    usersRepositoryFake = new UsersRepositoryFake();
+  });
+  it("should be able to delete an user", async () => {
+    expect(async () => {
+      const newUser = await usersRepositoryFake.create({
+        id: "12validid",
+        email: "validemail",
+        name: "validname",
+        password: "validpassword",
+      });
+      await deleteUsersUseCases.execute("12validid");
+    });
+  });
+  it("should not to be able to delete a no existent user", async () => {
+    expect(async () => {
+      await deleteUsersUseCases.execute("invalidid");
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
