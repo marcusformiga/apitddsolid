b4b6af7e0169ad7ee300380f0df9d743ee5a75c8
marcusformiga <kiko.antares@gmail.com>
2021-09-06 08:22:59 -0300

    test: criação dos testes de produtos

---
+++ src/__tests__/CreateProductsUseCases.spec.ts
@@ -0,0 +1,23 @@
+import { CreateProductDto } from "../module/users/entities/dto/CreateProductDto";
+import { ProductsRepositoryFake } from "../module/users/repositories/fakes/ProductsRepositoryFake";
+import { CreateProductsUseCases } from "../module/users/useCases/createProducts/CreateProductsUseCases";
+
+let createProductsUseCases: CreateProductsUseCases;
+let createProductRepositoryFake: ProductsRepositoryFake;
+describe("CreateProductsUseCases", () => {
+  beforeEach(() => {
+    createProductRepositoryFake = new ProductsRepositoryFake();
+    createProductsUseCases = new CreateProductsUseCases(
+      createProductRepositoryFake
+    );
+  });
+  it("should be able to create a new product", async () => {
+    const newProduct: CreateProductDto = {
+      name: "validname",
+      description: "validdescription",
+      price: 20,
+      quantity: 2,
+    };
+    await createProductsUseCases.execute(newProduct);
+  });
+});
