94df1eee1358c016748e64a0f101271785720440
marcusformiga <kiko.antares@gmail.com>
2021-09-06 08:27:02 -0300

    feature: migration de produtos

---
+++ src/shared/database/migrations/1630885216915-CreateProducts.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateProducts1630885216915 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "products",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "price",
+            type: "numeric",
+          },
+          {
+            name: "quantity",
+            type: "numeric",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("products");
+  }
+}
